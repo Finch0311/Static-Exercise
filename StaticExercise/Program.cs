8b328010cde39b7feef5d8b06beb63967c05e91a
Daniel Finch <xiphoid0311@gmail.com>
2023-07-17 19:49:00 -0500

    working temperature converter

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace StaticExercise
+﻿
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine($"Fahrenheit ---> Celsius : {TempConverter.FahrenheitToCelsius(32)}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Celsius ---> Fahrenheit : {TempConverter.CelsiusToFahrenheit(0)}");
+            Console.WriteLine();
+
         }
     }
 }
